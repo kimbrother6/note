11d5cc31ca5c78c2e03198096a28b7710f4f6a5a
kimbrother6 <81865593+kimbrother6@users.noreply.github.com>
2021-07-07 20:50:50 +0900

    Chane setting

--- note/settings.py
+++ note/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.testapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

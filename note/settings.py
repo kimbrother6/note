f54c40ee283cef573265470a6ceda5386ec219e3
kimbrother6 <81865593+kimbrother6@users.noreply.github.com>
2021-07-07 21:41:33 +0900

    Change back setting

--- note/settings.py
+++ note/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ['.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'apps.testapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

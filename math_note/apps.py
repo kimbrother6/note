34b1158c5c4aff048b7609456f3ca2d044a8def4
cubest_june <kim.hyung.june.08@gmail.com>
2021-07-07 22:35:09 +0900

    add math_note apps.py def ready

--- math_note/apps.py
+++ math_note/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class MathConfig(AppConfig):
     name = 'math'
     verbose_name = 'math_note'
+    def ready(self):
+        from matcher.background_tasks import close_timeout_games
+        from matcher.models import FourPlayerGame
+        # check if a player is not responding every 5 seconds
+        close_timeout_games(FourPlayerGame, repeat=5)

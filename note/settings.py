7971f5b6b7d5eea1d788eac7447eb7aa9158fb05
cubest_june <kim.hyung.june.08@gmail.com>
2021-07-07 20:44:10 +0900

    Change DEBUG Flase

--- note/settings.py
+++ note/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'aw97(fqlddg2s)kl!!4)trce9@ugcv%7n79f0_*)75o-qqr1xm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com']
 
 
 # Application definition
